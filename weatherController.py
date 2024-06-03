436d2c3e11d57d572eb61358005a5424332332d5
Игорь <vaskinin97@gmail.com>
2024-06-03 20:13:43 +0400

    git ignore update

--- weatherController.py
+++ weatherController.py
@@ -4,10 +4,11 @@ from api_key import key
 def getCityWeather(city):
     api_url = "http://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no"
     r = requests.get(api_url.format(key, city)).json()
+    print(r)
     if "error" in r:
         return None
     weather = {
-        'city' : city,
+        'city' : r["location"]["name"],
         'temperature' : r["current"]["temp_c"],
         'description' : r["current"]["condition"]["text"],
         'icon' : r["current"]["condition"]["icon"]
